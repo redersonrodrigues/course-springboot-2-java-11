239a235cfbcc6c064dbe1f73654e9336e8f1ea6f
Rederson Rodrigues <redersonatalibarodrigues@gmail.com>
2020-09-22 11:18:24 -0300

    Product

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -13,48 +13,93 @@ import javax.persistence.Transient;
 
 
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable{
-
+@Table(name = "tb_product")
+public class Product implements Serializable{
+	
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private Integer id;
 	private String name;
+	private String description;
+	private Double price;
+	private String imgURL;
+	
 	
 	@Transient
-	//associação
-	private Set<Product> products = new HashSet<>();
+	//asssociação
+	private Set <Category> categories= new HashSet<>();
 	
-	public Category() {}
+	
+	public Product() {}
 
-	public Category(Long id, String name) {
+
+	public Product(Integer id, String name, String description, Double price, String imgURL) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgURL = imgURL;
 	}
 
-	public Long getId() {
+
+	public Integer getId() {
 		return id;
 	}
 
-	public void setId(Long id) {
+
+	public void setId(Integer id) {
 		this.id = id;
 	}
+
 
 	public String getName() {
 		return name;
 	}
 
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	public Set<Product> getProducts() {
-		return products;
+
+
+	public String getDescription() {
+		return description;
 	}
-	
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public Double getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+
+	public String getImgURL() {
+		return imgURL;
+	}
+
+
+	public void setImgURL(String imgURL) {
+		this.imgURL = imgURL;
+	}
+
+
+	public Set<Category> getCategories() {
+		return categories;
+	}
+
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -62,6 +107,7 @@ public class Category implements Serializable{
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+
 
 	@Override
 	public boolean equals(Object obj) {
@@ -71,7 +117,7 @@ public class Category implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -79,5 +125,6 @@ public class Category implements Serializable{
 			return false;
 		return true;
 	}
+
 
 }
