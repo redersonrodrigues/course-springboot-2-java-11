156b092d4227e731689d72a024835dbd05e5bfbf
Rederson Rodrigues <redersonatalibarodrigues@gmail.com>
2020-09-22 13:30:53 -0300

    OrderItem, many-to-many association with extra attributes - Part 2

--- src/main/java/com/educandoweb/course/entities/OrderItem.java
+++ src/main/java/com/educandoweb/course/entities/OrderItem.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 import com.educandoweb.course.entities.pk.OrderItemPK;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 
 @Entity
@@ -17,7 +18,7 @@ public class OrderItem  implements Serializable {
 
 	
 	@EmbeddedId
-	private OrderItemPK id;
+	private OrderItemPK id = new OrderItemPK();
 	
 	
 	private Integer quantity;
@@ -33,6 +34,8 @@ public class OrderItem  implements Serializable {
 		this.price = price;
 	}
 	
+	
+	@JsonIgnore
 	public Order getOrder() {
 		return id.getOrder();
 	}
